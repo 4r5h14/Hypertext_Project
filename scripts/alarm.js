6286270e23157d2508c9eadc2b4b4862407ab459
Arshia Lodhi <arshia.lodhi@gmail.com>
2023-10-11 09:52:54 -0400

    extra polishing of backgrounds and text color

--- scripts/alarm.js
+++ scripts/alarm.js
@@ -4,10 +4,23 @@
 
 
 
-window.addEventListener('scroll', function(){
-  console.log('yes im scrolling')
-  let showMe = document.querySelector('.text');
-  showMe.style.opacity = '1';
-  this.setTimeout = "5s";
+//window.addEventListener('scroll', function(){
+ // console.log('yes im scrolling')
+  //let showMe = document.querySelector('.text');
+  //showMe.style.opacity = '1';
+  //this.setTimeout = "3s";
+
+//})
+
+
+
+let btn = document.querySelector('.text');
+
+//console.log('btn');
+
+
+btn.addEventListener('mouseover', function(){
+    let showMe = document.querySelector('.show-me');
+    showMe.style.opacity = '1';
 
 })
