8e6ba6febec2b98c68ae1897a4c1c6ae2ba381db
Arshia Lodhi <arshia.lodhi@gmail.com>
2023-10-11 10:17:32 -0400

    added bus gif

--- scripts/main.js
+++ scripts/main.js
@@ -5,26 +5,26 @@
 window.addEventListener("scroll", function(){
   let show2 = document.querySelector('.text');
   show2.style.opacity = '1';
-  this.setTimeout= "5s";
+  this.setTimeout = '5s';
   
 })
 
 window.addEventListener('scroll', function(){
   console.log('yes im scrolling')
   let showMe = document.querySelector('.fadein');
-  this.setTimeout= "10s";
+  this.setTimeout = '8s';
   showMe.style.opacity = '1';
 })
 
 window.addEventListener("scroll", function(){
   let show3 = document.querySelector('.fade');
-  this.setTimeout= "15s";
+  this.setTimeout = '10s';
   show3.style.opacity = '1';
 })
 
 window.addEventListener("scroll", function(){
   let show4 = document.querySelector('.fadeout');
-  this.setTimeout= "20s";
+  this.setTimeout = '14s';
   show4.style.opacity = '1';
 
 })
