7f30570e8327baa9cd291b4d8b16e87e883c25b0
Arshia Lodhi <arshia.lodhi@gmail.com>
2023-10-11 00:39:27 -0400

    added more content based on previous feedback

---
+++ scripts/alarm.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+window.addEventListener('scroll', function(){
+  console.log('yes im scrolling')
+  let showMe = document.querySelector('.text');
+  showMe.style.opacity = '1';
+  this.setTimeout = "5s";
+
+})
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,30 @@
-console.log('hi i m working :)')
+
+
+
+
+window.addEventListener("scroll", function(){
+  let show2 = document.querySelector('.text');
+  show2.style.opacity = '1';
+  this.setTimeout= "5s";
+  
+})
+
+window.addEventListener('scroll', function(){
+  console.log('yes im scrolling')
+  let showMe = document.querySelector('.fadein');
+  this.setTimeout= "10s";
+  showMe.style.opacity = '1';
+})
+
+window.addEventListener("scroll", function(){
+  let show3 = document.querySelector('.fade');
+  this.setTimeout= "15s";
+  show3.style.opacity = '1';
+})
+
+window.addEventListener("scroll", function(){
+  let show4 = document.querySelector('.fadeout');
+  this.setTimeout= "20s";
+  show4.style.opacity = '1';
+
+})
